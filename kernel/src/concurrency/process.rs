a5fde2fd617c4ba60254479a49cd1d358da735ae
ldq3 <dequan_li.cn@outlook.com>
2025-06-20 13:53:36 +0800

    temporary

--- kernel/src/concurrency/process.rs
+++ kernel/src/concurrency/process.rs
@@ -1,11 +1,18 @@
 use ones::{
-    concurrency::process::{ access, Lib as L, Process },
+    concurrency::process::{ Dependence, Lib as L, Process },
     memory::{ page::{ Lib as _, Table as PageTable }, Address },
     runtime::address_space::AddressSpace
 };
 use crate::{ memory::page::Lib as PageLib };
 
 pub struct Lib;
+
+impl Dependence for Lib {
+    #[inline]
+    fn copy_data(table: &mut PageTable, range: (usize, usize), data: &[u8]) {
+        PageLib::copy_data(table, range, data);
+    }
+}
 
 impl L for Lib {
     fn new(parent: Option<usize>, address_space: AddressSpace) -> usize {
@@ -39,22 +46,6 @@ impl L for Lib {
         PageLib::fixed_map(&mut page_table, segement.range.0 , frame_number, segement.flag);
 
         Process::new(None, address_space, page_table)
-    }
-
-    fn from_elf(parent: Option<usize>, elf: &[u8]) -> usize {
-        let (address_space, data_offset) = AddressSpace::from_elf(&elf);
-        let pid = Self::new(parent, address_space);
-
-        access(|manager| {
-            let process = manager.process[pid].as_mut().unwrap();
-            for i in 0..data_offset.len() { 
-                let segement= process.address_space.segement[i];
-
-                PageLib::copy_data(&mut process.page_table, segement.range, &elf[data_offset[i].0..data_offset[i].1]);
-            }
-        });
- 
-        pid
     }
 }
 
--- kernel/src/memory/page/mod.rs
+++ kernel/src/memory/page/mod.rs
@@ -7,6 +7,13 @@ const NUMBER_WIDTH: usize = 27;
 
 const OFFSET_WIDTH: usize = 12;
 
+## 物理地址
+const ADDRESS_WIDTH: usize = 56;
+
+const NUMBER_WIDTH: usize = 44;
+
+const OFFSET_WIDTH: usize = 12; 
+
 ## 页表项
 const FRAME_NUMER_WIDTH; // 参见 frame
 
@@ -15,14 +22,11 @@ const RSW_WIDTH: usize = 2;
 const FLAG_WIDTH: usize = 8;
 */
 
-pub mod frame;
 pub mod entry;
-
-use core::slice::from_raw_parts_mut;
 
 use alloc::vec::Vec;
 use ones::memory::{
-    page::{ entry::{ Lib as _, Entry }, frame::Frame, Dependence, Lib as L, Table },
+    page::{ entry::{ Lib as _, Entry }, Dependence, Lib as L },
     Flag,
 };
 use entry::EntryLib;
@@ -48,145 +52,25 @@ impl Dependence for Lib {
         3
     }
 
-    fn get_mut(table: &mut Table, page_num: usize) -> &mut Entry {
-        let index = Self::index(page_num);
-        let table = Self::as_table(table.root.number);
+    #[inline]
+    fn flag(entry: &Entry) -> Flag {
+        EntryLib::flag(entry)
+    }
 
-        let mut current_entry = &mut table[index[0]];
-        let frame_number = EntryLib::frame_number(current_entry);
-        let mut current_table = Self::as_table(frame_number);
+    #[inline]
+    fn new_entry(frame_num: usize, page_flag: Flag) -> Entry {
+        EntryLib::new(frame_num, page_flag)
+    }
 
-        for i in 1..(Self::conf() - 1) {
-            current_entry = &mut current_table[index[i]];
-            let frame_number= EntryLib::frame_number(current_entry);
-            current_table = Self::as_table(frame_number);
-        }
+    #[inline]
+    fn frame_number(entry: &Entry) -> usize {
+        EntryLib::frame_number(entry)
+    }
 
-        current_entry = &mut current_table[index[Self::conf() - 1]];
-
-        current_entry
+    #[inline]
+    fn set_flag(entry: &mut Entry, page_flag: Flag) {
+        EntryLib::flag_set(entry, page_flag);
     }
 }
 
-impl L for Lib {
-    fn map(table: &mut Table, page_num: usize, page_flag: Flag) {
-        let index = Self::index(page_num);
-        let root = Self::as_table(table.root.number);
-
-        let mut current_entry = &mut root[index[0]];
-        let mut current_table = if !EntryLib::flag(current_entry).is_valid() {
-            let frame = Frame::new();
-            let frame_number = frame.number;
-            *current_entry = EntryLib::new(frame_number, Flag::V);
-            table.frame.push(frame);
-
-            Self::as_table(frame_number)
-        } else {
-            let frame_number = EntryLib::frame_number(current_entry);
-            Self::as_table(frame_number)
-        };
-
-        for i in 1..(Self::conf() - 1) {
-            current_entry = &mut current_table[index[i]];
-            current_table = if !EntryLib::flag(current_entry).is_valid() {
-                let frame = Frame::new();
-                let frame_number = frame.number;
-                *current_entry = EntryLib::new(frame_number, Flag::V);
-                table.frame.push(frame);
-
-                Self::as_table(frame_number)
-            } else {
-                let frame_number = EntryLib::frame_number(current_entry);
-                Self::as_table(frame_number)
-            }
-        }
-
-        current_entry = &mut current_table[index[Self::conf() - 1]];
-        if !EntryLib::flag(current_entry).is_valid() {
-            let frame = Frame::new();
-            *current_entry = EntryLib::new(frame.number, Flag::V | page_flag);
-            table.frame.push(frame);
-        }
-    }
-
-    fn unmap(table: &mut Table, page_num: usize) {
-        let entry = Self::get_mut(table, page_num);
-
-        EntryLib::flag_set(entry, Flag::empty());
-    }
-
-    fn fixed_map(table: &mut Table, page_num: usize, frame_num: usize, page_flag: Flag) {
-        let index = Self::index(page_num);
-        let root = Self::as_table(table.root.number);
-
-        let mut current_entry = &mut root[index[0]];
-        let mut current_table = if !EntryLib::flag(current_entry).is_valid() {
-            let frame = Frame::new();
-            let frame_number = frame.number;
-            *current_entry = EntryLib::new(frame_number, Flag::V);
-            table.frame.push(frame);
-            Self::as_table(frame_number)
-        } else { 
-            let frame_number = EntryLib::frame_number(current_entry);
-            Self::as_table(frame_number)
-        };
-
-        for i in 1..(Self::conf() - 1) {
-            current_entry = &mut current_table[index[i]]; 
-            current_table = if !EntryLib::flag(current_entry).is_valid() {
-                let frame = Frame::new();
-                let frame_number = frame.number;
-                *current_entry = EntryLib::new(frame_number, Flag::V);
-                table.frame.push(frame);
-
-                Self::as_table(frame_number)
-            } else {
-                let frame_number = EntryLib::frame_number(current_entry);
-
-                Self::as_table(frame_number)
-            }
-        }
-
-        current_entry = &mut current_table[index[Self::conf() - 1]];
-        *current_entry = EntryLib::new(frame_num, page_flag | Flag::V);
-    }
-    
-    fn get(table: &mut Table, page_num: usize) -> (usize, Flag) {
-        let index = Self::index(page_num); let root = Self::as_table(table.root.number);
-
-        let mut current_entry = &mut root[index[0]];
-        let frame_number = EntryLib::frame_number(current_entry);
-        let mut current_table = Self::as_table(frame_number);
-
-        for i in 1..(Self::conf() - 1) {
-            current_entry = &mut current_table[index[i]];
-            let frame_number = EntryLib::frame_number(current_entry);
-            current_table = Self::as_table(frame_number);
-        }
-
-        current_entry = &mut current_table[index[Self::conf() - 1]];
-        (EntryLib::frame_number(current_entry), EntryLib::flag(current_entry))
-    }
-
-    fn copy_data(table: &mut Table, range: (usize, usize), data: &[u8]) {
-        use ones::memory::Address;
-        let page_size = Address::address(1);
-
-        let mut start: usize = 0;
-        let mut current = range.0;
-        let len = data.len();
-        loop {
-            let src = &data[start..len.min(start + page_size)];
-            let (frame_number, _) = Self::get(table, current);
-            let address = Address::address(frame_number);
-            let target = unsafe{ from_raw_parts_mut(address as *mut u8, src.len()) };
-            target.copy_from_slice(src);
-
-            start += page_size;
-            if start >= len {
-                break;
-            }
-            current += 1;
-        }
-    }
-}
+impl L for Lib { }
